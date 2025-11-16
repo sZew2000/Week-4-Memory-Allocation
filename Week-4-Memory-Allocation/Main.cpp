0cb8cd7b0c082bad6368932d80c633d6143488af
sZew2000 <sewerynmorys@outlook.com>
2025-11-16 20:44:45 +0000

    Created Class and functions - started loop to create 4 pointers in arr

--- Week-4-Memory-Allocation/Main.cpp
+++ Week-4-Memory-Allocation/Main.cpp
@@ -3,13 +3,44 @@
 #include <iostream>
 using namespace std; 
 
+class CModule
+{
+private : 
+
+	string moduleName; 
+	float moduleMark;
+
+public:
+
+	void setModule(string Name ,float Mark);
+	void outPutModule();
+};
+
+
+
+
 int main()
 {
+
+	 // Exercise 8: Module Marks (sparse array) 
 
 
 
 
 
 	_CrtDumpMemoryLeaks();
+
+}
+
+void CModule::setModule(string Name, float Mark)
+{
+	moduleName = Name; 
+	moduleMark = Mark;
+}
+
+void CModule::outPutModule()
+{
+
+	cout << moduleName << " " << moduleMark << endl;
 
 }
