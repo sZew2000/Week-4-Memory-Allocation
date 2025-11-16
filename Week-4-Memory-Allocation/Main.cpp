23d32cfc7933aca144b755e534b92f4b7f11e058
sZew2000 <sewerynmorys@outlook.com>
2025-11-16 16:56:13 +0000

    Added pinter struct & allocated name and age using arrow operator

--- Week-4-Memory-Allocation/Main.cpp
+++ Week-4-Memory-Allocation/Main.cpp
@@ -3,11 +3,21 @@
 #include <iostream>
 using namespace std; 
 
+struct cat
+{
+	string name;
+	int age;
+
+};
+
 int main()
 {
+	// Exercise 2: Cat Structure
+	
+	cat* cat_1 = new cat; 
 
-
-
+	cat_1->name = "sev"; 
+	cat_1->age = 25;
 
 
 	_CrtDumpMemoryLeaks();
